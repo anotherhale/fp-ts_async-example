d9fd645a6ea7ac69496dd33326162a1b93e29b8b
Andrew Hale <andrewhale@opalsoft.com>
2019-08-02 13:43:30 -0400

    Updated from feedback from SO - still not working

--- src/async-example.ts
+++ src/async-example.ts
@@ -1,48 +1,52 @@
 import { pipe } from 'fp-ts/lib/pipeable'
 import { getOrElse } from 'fp-ts/lib/Either'
-import { TaskEither, tryCatch, map,chain } from "fp-ts/lib/TaskEither";
+import { TaskEither, tryCatch, map, chain } from "fp-ts/lib/TaskEither";
 import * as T from 'fp-ts/lib/Task';
 import { promises as fsPromises } from 'fs';
 const yamlPromise = require('js-yaml-promise');
 
 // const path = require('path');
 export interface AppConfig {
-    service: {
-        interface: string
-        port: number
-    };
+  service: {
+    interface: string
+    port: number
+  };
 }
 
 function readFileAsyncAsTaskEither(path: string): TaskEither<unknown, string> {
-    return tryCatch(() => fsPromises.readFile(path, 'utf8'), reason => new Error(String(reason)))
+  return tryCatch(() => fsPromises.readFile(path, 'utf8'), reason => new Error(String(reason)))
 }
 
 function readYamlAsTaskEither(content: string): TaskEither<unknown, AppConfig> {
-    return tryCatch(() => yamlPromise.safeLoad(content), e=>e)
+  return tryCatch(() => yamlPromise.safeLoad(content), e => e)
 }
 
-function getConf(filePath:string):TaskEither<unknown,AppConfig>{
-    return pipe(
-        readFileAsyncAsTaskEither(filePath),
-        chain(readYamlAsTaskEither)
-    )
+function getConf(filePath: string): TaskEither<unknown, AppConfig> {
+  return pipe(
+    readFileAsyncAsTaskEither(filePath),
+    chain(readYamlAsTaskEither)
+  )
 }
 
 function printConfig(config: AppConfig): AppConfig {
-    console.log("AppConfig is: ", config);
-    return config;
-  }
-  
-  async function main(filePath: string): Promise<void> {
-    const program: T.Task<void> = pipe(
-      getConf(filePath),
-      map(printConfig),
-      getOrElse(e => {
-        return T.of(undefined);
-      })
-    );
-  
-    await program;
-  }
+  console.log("AppConfig is: ", config);
+  return config;
+}
 
-  main('./app-config.yaml')
+async function main(filePath: string): Promise<void> {
+  const program: T.Task<void> = pipe(
+    getConf(filePath),
+    // This line is not compiling:
+    // Argument of type '<E>(fa: TaskEither<E, AppConfig>) => TaskEither<E, AppConfig>' is not assignable to parameter of type '(a: TaskEither<unknown, AppConfig>) => Either<unknown, Task<any>>'.
+    //  Type 'TaskEither<unknown, AppConfig>' is not assignable to type 'Either<unknown, Task<any>>'.
+    //  Type 'TaskEither<unknown, AppConfig>' is missing the following properties from type 'Right<Task<any>>': _tag, rightts(2345)
+    map(printConfig),
+    getOrElse(e => {
+      return T.of(undefined);
+    })
+  );
+
+  await program;
+}
+
+main('./app-config.yaml')
