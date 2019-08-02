5a90821e7abe364877758440a0e4f4d0a887e7ca
Andrew Hale <andrewhale@opalsoft.com>
2019-08-02 14:53:32 -0400

    Used getOrElse from TaskEither - Resolved issue\!

--- src/async-example.ts
+++ src/async-example.ts
@@ -1,6 +1,5 @@
 import { pipe } from 'fp-ts/lib/pipeable'
-import { getOrElse } from 'fp-ts/lib/Either'
-import { TaskEither, tryCatch, map, chain } from "fp-ts/lib/TaskEither";
+import { TaskEither, tryCatch, chain, map, getOrElse } from "fp-ts/lib/TaskEither";
 import * as T from 'fp-ts/lib/Task';
 import { promises as fsPromises } from 'fs';
 const yamlPromise = require('js-yaml-promise');
@@ -11,13 +10,6 @@ export interface AppConfig {
     interface: string
     port: number
   };
-}
-
-const defaultConfig: AppConfig = {
-  service: {
-    interface: 'localhost',
-    port: 8080
-  }
 }
 
 function readFileAsyncAsTaskEither(path: string): TaskEither<unknown, string> {
@@ -43,17 +35,13 @@ function printConfig(config: AppConfig): AppConfig {
 async function main(filePath: string): Promise<void> {
   const program: T.Task<void> = pipe(
     getConf(filePath),
-    // The following line is not compiling:
-    // Argument of type '<E>(fa: TaskEither<E, AppConfig>) => TaskEither<E, AppConfig>' is not assignable to parameter of type '(a: TaskEither<unknown, AppConfig>) => Either<unknown, Task<any>>'.
-    //  Type 'TaskEither<unknown, AppConfig>' is not assignable to type 'Either<unknown, Task<any>>'.
-    //  Type 'TaskEither<unknown, AppConfig>' is missing the following properties from type 'Right<Task<any>>': _tag, rightts(2345)
     map(printConfig),
     getOrElse(e => {
-      return T.of(defaultConfig);
+      return T.of(undefined);
     })
   );
 
-  await program;
+  await program();
 }
 
 main('./app-config.yaml')
